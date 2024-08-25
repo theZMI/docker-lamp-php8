c182da1f3e0003b7cd8427402d8fc65282cc815b
theZMI <zmigpost@gmail.com>
2024-08-25 23:27:07 +0200

    Little code to check breakpoints

--- site/my_site/index.php
+++ site/my_site/index.php
@@ -1,4 +1,12 @@
 <?php
 
 $time = date('d-m-Y H:i:s');
-echo "Hello world. Current time = " . $time;
+echo "Hello world. Current time = {$time}<br>";
+
+$print = function($msg) {
+    echo "Message: $msg<br>";
+};
+
+for ($i = 0; $i < 10; $i++) {
+    $print($i);
+}
